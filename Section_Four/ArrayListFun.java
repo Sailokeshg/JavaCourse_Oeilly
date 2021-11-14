7469a2b04bec1a2f946c2d3732dad537ab24809d
Sailokeshg <18r01a0579@cmritonline.ac.in>
2021-11-14 23:27:55 +0530

    ArrayList intro

---
+++ Section_Four/ArrayListFun.java
@@ -0,0 +1,20 @@
+package Section_Four;
+
+import java.util.ArrayList;
+
+public class ArrayListFun {
+    public static void main(String[] args) {
+        ArrayList<String> namesList = new ArrayList<>();
+
+        namesList.add("Sai");
+        namesList.add("Lokesh");
+
+//        for (String st : namesList) {
+//            System.out.print(st+"\t");
+//        }
+
+        for(int i=0;i<namesList.size();i++){
+            System.out.println(namesList.get(i));
+        }
+    }
+}
---
+++ Section_Four/WrapperFun.java
@@ -0,0 +1,24 @@
+package Section_Four;
+
+import java.util.ArrayList;
+
+public class WrapperFun {
+    public static void main(String[] args){
+        ArrayList<Integer> myAl = new ArrayList<>();
+
+        myAl.add(10);
+        myAl.add(22);
+
+        for(int i:myAl){
+            System.out.println(i);
+        }
+        String someValue="120";
+        int numericValue = Integer.parseInt(someValue);
+        numericValue+=10;
+        System.out.println(numericValue);
+
+        String doubleString = "3.14159";
+        Double converted = Double.parseDouble(doubleString);
+        System.out.println(converted+1);
+    }
+}
