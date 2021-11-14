0b9a7d7146f4d1157ba52b0255f00de26c788bd3
Sailokeshg <18r01a0579@cmritonline.ac.in>
2021-11-14 19:52:35 +0530

    Random Number generator

---
+++ Section_Three/RandomFun.java
@@ -0,0 +1,18 @@
+package Section_Three;
+
+import java.util.Random;
+
+public class RandomFun {
+    public static void main(String[] args){
+        Random random = new Random();
+        int myRandomNumber;
+        myRandomNumber=random.nextInt();
+        System.out.println("numbrer is " + myRandomNumber);
+
+        myRandomNumber=random.nextInt(1000);
+        System.out.println("0 through 999 is " + myRandomNumber);
+
+        myRandomNumber=random.nextInt(1000)+1;
+        System.out.println("1 through 1000 is \t" + myRandomNumber);
+    }
+}
