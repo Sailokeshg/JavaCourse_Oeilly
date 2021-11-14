1aa67a468bcf4aad35ef2d39a6ed00c2169a1f08
Sailokeshg <18r01a0579@cmritonline.ac.in>
2021-11-14 19:18:37 +0530

    worked with while and for loops

---
+++ Section_Three/SumFun.java
@@ -0,0 +1,20 @@
+package Section_Three;
+
+import java.util.Scanner;
+
+public class SumFun {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int input;
+        int sum=0;
+        System.out.println("Enter  a non negative integer");
+        input=sc.nextInt();
+        while(input >=0){
+            sum+=input;
+            System.out.println("Enter  a non negative integer");
+            input=sc.nextInt();
+        }
+        System.out.println("sum of inputs is "+sum);
+        System.out.println("Wrong input");
+    }
+}
