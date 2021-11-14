386cb6a3b7607a7a58d47f792e284cec7065a4c0
Sailokeshg <18r01a0579@cmritonline.ac.in>
2021-11-14 22:52:23 +0530

    Arrays intro

---
+++ Section_Four/ArrayFun.java
@@ -0,0 +1,15 @@
+package Section_Four;
+
+public class ArrayFun {
+    public static void main(String[] args){
+        int[] myArray =new int[5];
+        myArray[0]=10;
+        myArray[1]=22;
+//        for(int i=0;i<myArray.length;i++){
+//            System.out.print(myArray[i]+"\t");
+//        }
+        for(int num:myArray){
+            System.out.println(num);
+        }
+    }
+}
---
+++ Section_Four/MoreArrayFun.java
@@ -0,0 +1,13 @@
+package Section_Four;
+
+public class MoreArrayFun {
+    public static void main(String[] args){
+        int[] myArray = new int[10];
+        for(int i=1;i<=10;i++){
+            myArray[i-1]=i;
+        }
+        for(int i:myArray){
+            System.out.print(i+"\t");
+        }
+    }
+}
